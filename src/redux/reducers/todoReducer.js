d46efa9e5cddd96e8376907e5bb7964be453d4a6
kruushik89 <kruushik89@gmail.com>
2021-07-22 13:11:19 +0300

    Підключаємо redux

---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    todos: []
+}
+
+export const todoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
